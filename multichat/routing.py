b010ff763a36386a3ee5f50acfdeacbd36c51020
alex yang <alexalexyang@gmail.com>
2018-05-05 17:19:00 +0200

    trying StaticFilesConsumer() in routing.py for deployment

--- multichat/routing.py
+++ multichat/routing.py
@@ -6,13 +6,17 @@ from channels.auth import AuthMiddlewareStack
 
 from chat.consumers import ChatConsumer
 
-
+# added
+from channels.staticfiles import StaticFilesConsumer
 # The channel routing defines what connections get handled by what consumers,
 # selecting on either the connection type (ProtocolTypeRouter) or properties
 # of the connection's scope (like URLRouter, which looks at scope["path"])
 # For more, see http://channels.readthedocs.io/en/latest/topics/routing.html
 application = ProtocolTypeRouter({
 
+    #added
+
+    'http.request': StaticFilesConsumer(),
     # Channels will do this for you automatically. It's included here as an example.
     # "http": AsgiHandler,
 
