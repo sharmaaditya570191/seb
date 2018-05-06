c6f4c2ecdff4dc6f0ccb6a88ed08c84f2f76ebff
alex yang <alexalexyang@gmail.com>
2018-05-06 22:40:21 +0200

    changed all 'multichat' to 'seb'

--- seb/wsgi.py
+++ seb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for multichat project.
+WSGI config for seb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multichat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seb.settings")
 
 application = get_wsgi_application()
